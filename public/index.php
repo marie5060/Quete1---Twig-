5d9dbf81b80226b66f3f0824f3d57ba8c6b617d7
marie5060 <marie5060@hotmail.com>
2022-10-14 11:26:24 +0200

    visible list in index.php

--- public/index.php
+++ public/index.php
@@ -2,8 +2,9 @@
 
 // Get a $twig object from this file.
 require_once __DIR__ . '/../config/twig.php';
-
+require_once __DIR__ . '/../src/products.php';
 
 $name = 'Wilder';
 
 echo $twig->render('home.html.twig', ['name' => $name]);
+echo $twig->render('products.html.twig', ['products' => $products]);
---
+++ src/products.php
@@ -0,0 +1,6 @@
+<?php
+
+// Get a $twig object from this file.
+require_once __DIR__ . '/../config/twig.php';
+
+$products = ['guitare', 'bass', 'bonjo', 'cithare', 'lyre', 'harpe'];
